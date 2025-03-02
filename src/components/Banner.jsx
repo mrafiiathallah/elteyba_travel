d0ff5a82ac81a2b0a3326d97b57330275f375847
Mahennatadev <121371000+Mahennatadev@users.noreply.github.com>
2025-03-03 00:12:42 +0700

    Ignore node_modules

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,16 @@
 import { motion } from 'framer-motion';
 
-const Banner = ({ image, title, subtitle }) => {
+const Banner = ({ image, title, subtitle, description }) => {
   return (
     <div 
-      className="banner relative h-[500px] flex items-center justify-center"
+      className="banner relative min-w-full h-[670px] flex items-center justify-center overflow-hidden"
       style={{ 
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${image})` 
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${image})`,
       }}
     >
       <div className="text-center text-white z-10 px-4">
         <motion.h1 
-          className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4"
+          className="text-xl md:text-3xl max-w-4xl mx-auto"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
@@ -18,12 +18,20 @@ const Banner = ({ image, title, subtitle }) => {
           {title}
         </motion.h1>
         <motion.p 
-          className="text-xl md:text-2xl max-w-3xl mx-auto"
+          className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
           {subtitle}
+        </motion.p>
+        <motion.p
+          className="text-lg md:text-xl max-w-3xl mx-auto mt-2"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+        >
+          {description}
         </motion.p>
       </div>
     </div>
