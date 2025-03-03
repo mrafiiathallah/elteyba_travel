b1e096873d1074ae4f5bad7fbd7297c86e2ab505
Mahennatadev <121371000+Mahennatadev@users.noreply.github.com>
2025-03-04 01:34:52 +0700

    Menyelesaikan revisi Navbar, merubah warna navbar dan login, merubah warna toggle menjadi putih

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, NavLink, useLocation } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -15,7 +28,11 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white sticky top-0 z-50">
+    <nav
+      className={`fixed top-0 w-full z-50 transition-all duration-300 ${
+        scrolling ? "bg-gray-400 shadow-md" : "bg-transparent"
+      }`}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
@@ -27,49 +44,49 @@ const Navbar = () => {
           <div className="hidden md:flex items-center space-x-8">
             <NavLink 
               to="/elteyba_travel" 
-              className={`nav-link text-gray-700 hover:text-primary ${isActive('/elteyba_travel') ? 'active-nav-link font-medium' : ''}`}
+              className={`nav-link text-white ${isActive('/elteyba_travel') ? 'active-nav-link font-medium' : ''}`}
             >
               Home
             </NavLink>
             <NavLink 
               to="/destinations" 
-              className={`nav-link text-gray-700 hover:text-primary ${isActive('/destinations') ? 'active-nav-link font-medium' : ''}`}
+              className={`nav-link text-white ${isActive('/destinations') ? 'active-nav-link font-medium' : ''}`}
             >
               Destinations
             </NavLink>
             <NavLink 
               to="/packages" 
-              className={`nav-link text-gray-700 hover:text-primary ${isActive('/packages') ? 'active-nav-link font-medium' : ''}`}
+              className={`nav-link text-white ${isActive('/packages') ? 'active-nav-link font-medium' : ''}`}
             >
               Hajj & Umrah Packages
             </NavLink>
             {/* <NavLink 
               to="/pricing" 
-              className={`nav-link text-gray-700 hover:text-primary ${isActive('/pricing') ? 'active-nav-link font-medium' : ''}`}
+              className={`nav-link text-white ${isActive('/pricing') ? 'active-nav-link font-medium' : ''}`}
             >
               Pricing
             </NavLink> */}
             <NavLink 
               to="/testimonials" 
-              className={`nav-link text-gray-700 hover:text-primary ${isActive('/testimonials') ? 'active-nav-link font-medium' : ''}`}
+              className={`nav-link text-white ${isActive('/testimonials') ? 'active-nav-link font-medium' : ''}`}
             >
               Testimonials
             </NavLink>
             <NavLink 
               to="/contact" 
-              className={`nav-link text-gray-700 hover:text-primary ${isActive('/contact') ? 'active-nav-link font-medium' : ''}`}
+              className={`nav-link text-white ${isActive('/contact') ? 'active-nav-link font-medium' : ''}`}
             >
               Contact Us
             </NavLink>
           </div>
           
           <div className="hidden md:flex items-center space-x-6">
-            <Link to="/login" className="text-gray-700 hover:text-primary">
+            <Link to="/login" className="text-white font-semibold hover:text-accent transform duration-300">
               Login
             </Link>
             <Link 
               to="/signup" 
-              className="border-transparent border-2 bg-accent hover:bg-white hover:text-accent hover:border-accent text-white font-semibold px-8 py-2 rounded-3xl transition duration-500"
+              className="border-transparent border-2 bg-accent hover:bg-transparent hover:text-accent hover:border-accent text-white font-semibold px-8 py-2 rounded-3xl transition duration-500"
             >
               Sign Up
             </Link>
@@ -78,7 +95,7 @@ const Navbar = () => {
           <div className="md:hidden flex items-center">
             <button
               onClick={toggleMenu}
-              className="text-gray-700 hover:text-primary focus:outline-none"
+              className="text-white hover:text-primary focus:outline-none"
             >
               {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
             </button>
